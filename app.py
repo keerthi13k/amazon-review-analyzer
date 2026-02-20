26dcb5ce77b2178f7dbb4f82bf23794d1def6991
keerthi13k <145902697+keerthi13k@users.noreply.github.com>
2026-02-20 21:38:58 +0530

    Remove unused TextBlob import
    Removed import of TextBlob from app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from textblob import TextBlob
 from collections import Counter
 import plotly.express as px
 import plotly.graph_objects as go
